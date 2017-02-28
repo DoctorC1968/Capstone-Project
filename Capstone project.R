bc16b5af13b23777e71af653001bd9d8f295423c
DoctorC1968 <hero11057973@yahoo.com>
2017-02-27 20:32:51 -0600

    Add files via upload

--- Capstone project.R
+++ Capstone project.R
@@ -7,10 +7,14 @@ consumer_complaints<-read.csv("Consumer_Complaints.csv",stringsAsFactors = FALSE
 head(consumer_complaints)
 str(consumer_complaints)
 
+consumer_complaints_banksOnly<-consumer_complaints[consumer_complaints$Product %in% 
+                                                  c("Consumer Loan","Bank account or service"),]
+str(consumer_complaints_banksOnly)#108,764 obs
+
 library(dplyr)
 
-min(consumer_complaints$Date.received)#min date is 1/1/12
-max(consumer_complaints$Date.received)#max date is 9/9/16
+min(consumer_complaints_banksOnly$Date.received)#min date is 1/1/13
+max(consumer_complaints_banksOnly$Date.received)#max date is 9/9/16
 
 #Next read in the equity capital information for the banks. The purpose is to 
 #assign a size to the banks so that we can scale their complaint counts accordingly
@@ -87,20 +91,24 @@ str(all_ranked_banks)
 all_ranked_banks2<-subset(all_ranked_banks,select=-Date)
 rm(all_ranked_banks)
 
-write.csv(all_ranked_banks2, file = "banks_ranked_by_equity_capital.csv")
+str(all_ranked_banks2)#133880 obs
+#write.csv(all_ranked_banks2, file = "banks_ranked_by_equity_capital.csv")
+max(all_ranked_banks2$Rank) #7366
 
+maxrank<-100
+
+all_ranked_banks2_top<-all_ranked_banks2[all_ranked_banks2$Rank<=maxrank,]
+str(all_ranked_banks2_top)#2000 obs
 
 #Extract unique bank names. Bank names will be merge keys. We want to experiment with
 #some "fuzzy" merge techniques. 
-unique_banks_from_complaints<-unique(consumer_complaints$Company)
-length(unique_banks_from_complaints)#4060
-unique_banks_from_complaints<-sort(unique_banks_from_complaints)
+unique_banks_from_complaints<-unique(consumer_complaints_banksOnly$Company)
+length(unique_banks_from_complaints)#1130
 unique_banks_from_complaints[1:20]
 
-unique_banks_from_ranked_banks<-unique(all_ranked_banks2$Bank.Name)
-length(unique_banks_from_ranked_banks)#6722
-unique_banks_from_ranked_banks<-sort(unique_banks_from_ranked_banks)
-unique_banks_from_ranked_banks[1:20]
+unique_banks_from_ranked_banks_top<-unique(all_ranked_banks2_top$Bank.Name)
+length(unique_banks_from_ranked_banks_top)#136 banks
+unique_banks_from_ranked_banks_top
 
 #Create the Cartesian product of the two sets of unique bank names. When we get that 
 #done we'll find the Levenstein distances for all bank name pairs. 
@@ -193,44 +201,110 @@ library(stringdist)
 
 distances <-data.frame(bank1=character(),bank2=character(),cleanedUpBank1=character(),
                        cleanedUpBank2=character(), d=integer(),stringsAsFactors = FALSE)
-threshold<-0.2
+threshold<-0.42
+
 for (i in 1:length(unique_banks_from_complaints)){
-  for (j in 1:length(unique_banks_from_ranked_banks)){
+  for (j in 1:length(unique_banks_from_ranked_banks_top)){
 #Here we remove words from the bank names that give no information about the bank's identity. 
 #These words are sometimes called "stop words"
-    CleanedUpBankName1<-gsub(' BANK','',toupper(unique_banks_from_complaints[i]))
+    CleanedUpBankName1<-gsub(' BANKS ',' ',toupper(unique_banks_from_complaints[i]))
+    CleanedUpBankName1<-gsub('BANKS ','',CleanedUpBankName1)
+    CleanedUpBankName1<-gsub(' BANKS','',CleanedUpBankName1)
+    CleanedUpBankName1<-gsub(' BANK ',' ',CleanedUpBankName1)
+    CleanedUpBankName1<-gsub('BANK ','',CleanedUpBankName1)
+    CleanedUpBankName1<-gsub(' BANK','',CleanedUpBankName1)
+    
+    CleanedUpBankName1<-gsub('BANK','',CleanedUpBankName1)
     CleanedUpBankName1<-gsub(' INC.','',CleanedUpBankName1)
-    CleanedUpBankName1<-gsub(' BANCO','',CleanedUpBankName1)
-    CleanedUpBankName1<-gsub(' FINANCIAL','',CleanedUpBankName1)
-    CleanedUpBankName1<-gsub(' FINANCE','',CleanedUpBankName1)
-    CleanedUpBankName1<-gsub(' CAPITAL','',CleanedUpBankName1) 
-    CleanedUpBankName1<-gsub(' SERVICES','',CleanedUpBankName1)
-    CleanedUpBankName2<-gsub(' BANK','',toupper(unique_banks_from_ranked_banks[j]))
+    CleanedUpBankName1<-gsub('COMPANY','',CleanedUpBankName1)
+    CleanedUpBankName1<-gsub(' CO[.]','',CleanedUpBankName1)
+    CleanedUpBankName1<-gsub('BANCO','',CleanedUpBankName1)
+    CleanedUpBankName1<-gsub('FINANCIAL','',CleanedUpBankName1)
+    CleanedUpBankName1<-gsub('FINANCE','',CleanedUpBankName1)
+    CleanedUpBankName1<-gsub('CAPITAL','',CleanedUpBankName1) 
+    CleanedUpBankName1<-gsub('SERVICES','',CleanedUpBankName1)
+    CleanedUpBankName1<-gsub('CORPORATION','',CleanedUpBankName1)
+    CleanedUpBankName1<-gsub(' CITIZENS ',' ',CleanedUpBankName1)
+    CleanedUpBankName1<-gsub('CITIZENS ','',CleanedUpBankName1)
+    CleanedUpBankName1<-gsub(' CITIZENS','',CleanedUpBankName1)
+    CleanedUpBankName1<-gsub('FIRST ','',CleanedUpBankName1)
+    CleanedUpBankName1<-gsub(' TRUST ',' ',CleanedUpBankName1)
+    CleanedUpBankName1<-gsub('TRUST ','',CleanedUpBankName1)
+    CleanedUpBankName1<-gsub(' TRUST','',CleanedUpBankName1)
+    CleanedUpBankName1<-gsub(',','',CleanedUpBankName1)
+    CleanedUpBankName1<-gsub('&','',CleanedUpBankName1)
+    CleanedUpBankName1<-gsub('[.]','',CleanedUpBankName1)
+    CleanedUpBankName1<-gsub(' PUERTO RICO ',' ',CleanedUpBankName1)
+    CleanedUpBankName1<-gsub(' PUERTO RICO','',CleanedUpBankName1)
+    CleanedUpBankName1<-gsub('PUERTO RICO ','',CleanedUpBankName1) 
+    CleanedUpBankName1<-gsub(' OF THE ','',CleanedUpBankName1) 
+    CleanedUpBankName1<-gsub(' OF ','',CleanedUpBankName1) 
+    CleanedUpBankName1<-gsub(' THE ','',CleanedUpBankName1)
+    CleanedUpBankName1<-gsub(' AND ','',CleanedUpBankName1) 
+    
+    CleanedUpBankName2<-gsub(' BANKS ',' ',toupper(unique_banks_from_ranked_banks_top[j]))
+    CleanedUpBankName2<-gsub('BANKS ','',CleanedUpBankName2)
+    CleanedUpBankName2<-gsub(' BANKS','',CleanedUpBankName2)
+    CleanedUpBankName2<-gsub(' BANK ',' ',CleanedUpBankName2)
+    CleanedUpBankName2<-gsub('BANK ','',CleanedUpBankName2)
+    CleanedUpBankName2<-gsub(' BANK','',CleanedUpBankName2)
+    
+    CleanedUpBankName2<-gsub('BANK','',CleanedUpBankName2)
     CleanedUpBankName2<-gsub(' INC.','',CleanedUpBankName2)
-    CleanedUpBankName2<-gsub(' BANCO','',CleanedUpBankName2)
-    CleanedUpBankName2<-gsub(' FINANCIAL','',CleanedUpBankName2)
-    CleanedUpBankName2<-gsub(' FINANCE','',CleanedUpBankName2)
-    CleanedUpBankName2<-gsub(' CAPITAL','',CleanedUpBankName2)
-    CleanedUpBankName2<-gsub(' SERVICES','',CleanedUpBankName2)
-  
+    CleanedUpBankName2<-gsub('COMPANY','',CleanedUpBankName2)
+    CleanedUpBankName2<-gsub(' CO[.]','',CleanedUpBankName2)
+    CleanedUpBankName2<-gsub('BANCO','',CleanedUpBankName2)
+    CleanedUpBankName2<-gsub('FINANCIAL','',CleanedUpBankName2)
+    CleanedUpBankName2<-gsub('FINANCE','',CleanedUpBankName2)
+    CleanedUpBankName2<-gsub('CAPITAL','',CleanedUpBankName2)
+    CleanedUpBankName2<-gsub('SERVICES','',CleanedUpBankName2)
+    CleanedUpBankName2<-gsub('CORPORATION','',CleanedUpBankName2)
+    CleanedUpBankName2<-gsub(' CITIZENS ',' ',CleanedUpBankName2)
+    CleanedUpBankName2<-gsub('CITIZENS ','',CleanedUpBankName2)
+    CleanedUpBankName2<-gsub(' CITIZENS','',CleanedUpBankName2)
+    CleanedUpBankName2<-gsub('FIRST ','',CleanedUpBankName2)
+    CleanedUpBankName2<-gsub(' TRUST ',' ',CleanedUpBankName2)
+    CleanedUpBankName2<-gsub('TRUST ','',CleanedUpBankName2)
+    CleanedUpBankName2<-gsub(' TRUST','',CleanedUpBankName2)
+    CleanedUpBankName2<-gsub(',','',CleanedUpBankName2)
+    CleanedUpBankName2<-gsub('&','',CleanedUpBankName2)
+    CleanedUpBankName2<-gsub('[.]','',CleanedUpBankName2)
+    CleanedUpBankName2<-gsub(' PUERTO RICO ',' ',CleanedUpBankName2)
+    CleanedUpBankName2<-gsub(' PUERTO RICO','',CleanedUpBankName2)
+    CleanedUpBankName2<-gsub('PUERTO RICO ','',CleanedUpBankName2) 
+    CleanedUpBankName2<-gsub(' OF THE ','',CleanedUpBankName2)
+    CleanedUpBankName2<-gsub(' OF ','',CleanedUpBankName2) 
+    CleanedUpBankName2<-gsub(' THE ','',CleanedUpBankName2)
+    CleanedUpBankName2<-gsub(' AND ','',CleanedUpBankName2) 
+    
   #Distance between 2 bank names is defined as longest common substring distance, 
   #normalized by length of cleaned up bank name
-    dist<-stringdist(CleanedUpBankName1,CleanedUpBankName2,method="lcs")/nchar(CleanedUpBankName1)
-
-
+    if (max(nchar(CleanedUpBankName1),nchar(CleanedUpBankName2))<=3){
+      dist<-stringdist(CleanedUpBankName1,CleanedUpBankName2,method="lv")
+    }
+    else if ((regexpr("AMERICA",CleanedUpBankName1)[1]>0 & regexpr("COMERICA",CleanedUpBankName2)[1]>0)|
+             (regexpr("COMERICA",CleanedUpBankName1[1]>0)& regexpr("AMERICA",CleanedUpBankName2)[1]>0)){
+      dist<-threshold+1}
+    else {
+      dist<-stringdist(CleanedUpBankName1,CleanedUpBankName2,method="lv")/min(nchar(CleanedUpBankName1),nchar(CleanedUpBankName2))
+    }
     if (dist <= threshold){
-      newrow<-data.frame(bank1=unique_banks_from_complaints[i],bank2=unique_banks_from_ranked_banks[j],
-                       cleanedUpBank1=CleanedUpBankName1,cleanedUpBank2=CleanedUpBankName2,d=dist, 
-                       stringsAsFactors = FALSE)
+      newrow<-data.frame(bank1=unique_banks_from_complaints[i],
+                         bank2=unique_banks_from_ranked_banks_top[j],
+                         cleanedUpBank1=CleanedUpBankName1,cleanedUpBank2=CleanedUpBankName2,
+                         d=dist, stringsAsFactors = FALSE)
       distances<-rbind(distances,newrow)
       }
   }
 }
-#There were only about 100 pairs of matched names out of 4060 unique bank names from the complaints data.
-#This suggest to me that the two sets of bank names actually have a very small intersection. 
+
+write.csv(distances,file="Bank name pairs and distances.csv")
+
 
 #take a random sample of size 50000 from a dataset mydata 
 #sample without replacement
-consumer_complaints_samp50000 <- consumer_complaints[sample(1:nrow(consumer_complaints), 
-                                                     50000, replace=FALSE),]
-write.csv(consumer_complaints_samp50000, file = "consumer_complaints_samp50000.csv")
+#consumer_complaints_samp50000 <- consumer_complaints[sample(1:nrow(consumer_complaints), 50000, replace=FALSE),]
+#write.csv(consumer_complaints_samp50000, file = "consumer_complaints_samp50000.csv")
+
+
+
